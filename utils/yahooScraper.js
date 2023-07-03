dc4a07ddd141b83a75c648cfbee298ac5efeb905
Sampsa Heinonen <sampsavh@gmail.com>
2023-07-04 00:11:35 +0300

    added user-agent

--- utils/yahooScraper.js
+++ utils/yahooScraper.js
@@ -5,26 +5,42 @@ const yahooHeaderContent = ['^GSPC', '^DJI', '^IXIC', '^RUT', 'GC=F', 'SI=F', 'E
                             '^TNX', 'GBPUSD=X', 'JPY=X', 'BTC-USD', '^CMC200', '^FTSE',
                             'BTC-USD', '^N225']
 
-getData = function(ticker){
-    return fetch(`https://finance.yahoo.com/quote/${ticker}/`)
-        .then(response => response.text())
-        .then(response => {
-            let changePercent = 0
-            const $ = cheerio.load(response);
-            console.log('Finding ticker ' + ticker)
-            
-            let value = $(`fin-streamer[data-symbol=${ticker}]`).first().attr('value');  
-            changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(2)`).first().attr('value') * 100;
+getData = function(ticker) {
+    const headers = {
+      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+      'Accept': '*/*',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'en-US,en;q=0.9,fi-FI;q=0.8,fi;q=0.7',
+      'Referer': `https://finance.yahoo.com/quote/${ticker}?p=${ticker}`,
+      'Upgrade-Insecure-Requests': '1',
+      'Origin': 'https://finance.yahoo.com',
+      'Sec-Fetch-Site': 'same-site'
+    };
+  
+    const requestOptions = {
+      headers: headers
+    };
+  
+    return fetch(`https://finance.yahoo.com/quote/${ticker}/`, requestOptions)
+      .then(response => response.text())
+      .then(response => {
+        let changePercent = 0;
+        const $ = cheerio.load(response);
+        console.log('Finding ticker ' + ticker);
+  
+        let value = $(`fin-streamer[data-symbol=${ticker}]`).first().attr('value') * 1;
+        changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(2)`).first().attr('value') * 100;
+  
+        console.log('Value: ' + value + '; Changepercent ' + changePercent);
+  
+        return {
+          ticker: ticker,
+          value: value,
+          changePercent: changePercent
+        };
+      })
+      .catch(error => console.log('Error while scraping Yahoo: ' + error.message));
+  };
 
-            // if (yahooHeaderContent.includes(ticker)){
-            //     changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(2)`).first().attr('value') * 100;
-            // } else {
-            //     changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(2)`).first().attr('value') * 100;
-            // }
-            
-            console.log('Value: ' + value + '; Changepercent ' + changePercent);
-            return [value, changePercent];
-        },
-        error => console.log('Error while scraping yahoo ' + error.message))        
-}
+
 exports.getData = getData;
--- utils/yahooTickerQuery.js
+++ utils/yahooTickerQuery.js
@@ -1,12 +1,24 @@
 
 
 searchTickers = async function(ticker){
+
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9,fi-FI;q=0.8,fi;q=0.7',
+        'Referer': `https://finance.yahoo.com/quote/${ticker}?p=${ticker}`,
+        'Upgrade-Insecure-Requests': '1',
+        'Origin': 'https://finance.yahoo.com',
+        'Sec-Fetch-Site': 'same-site'
+      };
+    
+    const requestOptions = {
+    headers: headers
+    };
+
     const url = `https://query1.finance.yahoo.com/v1/finance/search?q=${ticker}&lang=en-US&region=US&quotesCount=6&newsCount=2&listsCount=2&enableFuzzyQuery=false&quotesQueryId=tss_match_phrase_query&multiQuoteQueryId=multi_quote_single_token_query&newsQueryId=news_cie_vespa&enableCb=true&enableNavLinks=true&enableEnhancedTrivialQuery=true&enableResearchReports=true&researchReportsCount=2`;
-    return fetch(url, {
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
-        }
-    })
+    return fetch(url, params)
         .then(response => response.json())
         .then(response => {
             response = response.quotes;
@@ -14,5 +26,6 @@ searchTickers = async function(ticker){
             return response
         }, error => console.log('Error while searching tickers ' + error.message))
 }
+
 
 exports.searchTickers = searchTickers;
