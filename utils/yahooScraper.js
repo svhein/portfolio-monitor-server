7956d149a0865a2f00ca3d04b59238d4077c82e9
Sampsa Heinonen <sampsavh@gmail.com>
2023-07-03 15:17:46 +0300

    returns changepercent correctly

--- utils/yahooScraper.js
+++ utils/yahooScraper.js
@@ -6,25 +6,25 @@ const yahooHeaderContent = ['^GSPC', '^DJI', '^IXIC', '^RUT', 'GC=F', 'SI=F', 'E
                             'BTC-USD', '^N225']
 
 getData = function(ticker){
-    return axios(`https://finance.yahoo.com/quote/${ticker}/`)
+    return fetch(`https://finance.yahoo.com/quote/${ticker}/`)
+        .then(response => response.text())
         .then(response => {
-            const html = response.data
             let changePercent = 0
-            const $ = cheerio.load(html);
+            const $ = cheerio.load(response);
             console.log('Finding ticker ' + ticker)
             
             let value = $(`fin-streamer[data-symbol=${ticker}]`).first().attr('value');  
+            changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(2)`).first().attr('value') * 100;
 
-            if (yahooHeaderContent.includes(ticker)){
-                changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(2)`).first().attr('value');
-            } else {
-                changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(3)`).first().attr('value') * 100;
-            }
+            // if (yahooHeaderContent.includes(ticker)){
+            //     changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(2)`).first().attr('value') * 100;
+            // } else {
+            //     changePercent = $(`fin-streamer[data-symbol=${ticker}]:nth-child(2)`).first().attr('value') * 100;
+            // }
             
-    
             console.log('Value: ' + value + '; Changepercent ' + changePercent);
             return [value, changePercent];
         },
-        error => console.log('VIRHE ' + error.message))        
+        error => console.log('Error while scraping yahoo ' + error.message))        
 }
 exports.getData = getData;
